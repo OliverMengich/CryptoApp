0ca6ae76ad757fc73e66d991e1183392ed23ab66
OliverMengich <kemeioliver2000@gmail.com>
2023-06-07 22:55:01 +0300

    Downloads

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, ImageBackground,SafeAreaView } from 'react-native';
+import Button from './components/Button';
+import React, { useState } from 'react';
+import AuthPage from './screens/AuthPage';
+import MainPage from './screens/MainPage';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [currentAccount, setCurrentAccount] = useState("");
+	let screen = <MainPage />;
+
+	return (
+		<>
+			<StatusBar style="dark" />
+			<ImageBackground style={{flex:1}} imageStyle={{opacity: .5}} resizeMode='cover' source={require('./assets/background.jpg')}>
+				<SafeAreaView style={styles.container}>
+					{screen}
+				</SafeAreaView>
+			</ImageBackground>
+		</>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	backgroundImage:{
+		// opacity: .15,
+		flex:1,
+		// backgroundColor:'rgba(0,0,0,.5)'
+	}
 });
