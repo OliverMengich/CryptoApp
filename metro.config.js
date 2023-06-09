d81fa2fc007d92bc9a201a306614de47cda97239
OliverMengich <kemeioliver2000@gmail.com>
2023-06-09 20:48:27 +0300

    Modifies and install dependency

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,17 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
 module.exports = {
-    resolver: {
-      extraNodeModules: require("expo-crypto-polyfills"),
-    },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
 };
